65d2abef0aef32ac296db3492a9789a0f1d1bdba
Samuel Kraus <53798819+Skraus5628@users.noreply.github.com>
2020-08-30 19:00:25 -0500

    added a binary search tree to optimize names.py

--- names/names.py
+++ names/names.py
@@ -1,4 +1,5 @@
 import time
+from binary_search_tree import BinarySearchTree #pylint: disable=import-error
 
 start_time = time.time()
 
@@ -13,14 +14,28 @@ f.close()
 duplicates = []  # Return the list of duplicates in this data structure
 
 # Replace the nested for loops below with your improvements
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+bst = BinarySearchTree()
+#for name_1 in names_1:
+    #for name_2 in names_2:
+        #if name_1 == name_2:
+            #duplicates.append(name_1)
+
+for name_2 in names_2:
+    if bst.contains(name_2):
+        duplicates.append(name_2)
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
 print (f"runtime: {end_time - start_time} seconds")
+
+
+#Pre-op code
+    #TC: O(n^2)
+    #RT: 4.734 seconds
+
+#Post-OP code:
+    #*added Binary Search Tree
+    #RTC: 0.0109 seconds
 
 # ---------- Stretch Goal -----------
 # Python has built-in tools that allow for a very efficient approach to this problem
